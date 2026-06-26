25a7d7f840bfe6a72f760d73ba1a961522f160e1
Ashutosh <singhcommerceclassess@gmail.com>
2026-06-27 02:11:50 +0530

    fix: Import missing Link in App.jsx to fix blank screen crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 // Layout & Core Components
